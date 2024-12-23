713ec86eeba38968aaeffc35110a813251ed00b2
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-23 22:47:56 +0530

    feat: varibles in java

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+
+    public static void main(String args[]) {
+        System.out.print("this is varibales \n");
+
+        // variables
+
+        String name = "this is string";
+
+        int a = 25;
+        int b = 556;
+
+        double price = 45.80;
+
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
+    }
+}
