68486dd1133ff35522939475c3f384f47b62f426
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-24 23:06:05 +0530

    feat: pattern solid ractangle

---
+++ patterns/solidRactangle.java
@@ -0,0 +1,21 @@
+package patterns;
+
+import loops.forLoop;
+
+public class solidRactangle {
+    public static void main(String args[]) {
+        // Solid ractangle
+
+        // ****
+        // ****
+        // ****
+        // ****
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 1; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println("\n");
+        }
+    }
+}
