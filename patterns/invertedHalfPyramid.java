72271f4d6f8b51b095fda94da66ff24fc3c96f30
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-26 01:21:34 +0530

    feat: invreted half-pyramid 80 degree

--- patterns/invertedHalfPyramid.java
+++ patterns/invertedHalfPyramid.java
@@ -10,12 +10,34 @@ public class invertedHalfPyramid {
         // *
         int n = 5;
 
+        System.out.println("Inverted Pyramid");
         // outer loop
         for (int i = n; i >= 1; i--) {
-            //Inner loop
+            // Inner loop
             for (int j = 1; j <= i; j++) {
                 System.out.print(" *");
             }
+            System.out.println();
+        }
+
+        // Inverted Pyramid 80 degree --> 2 inner loops
+        //         *
+        //       * *
+        //     * * *
+        //   * * * *
+        // * * * * *
+        System.out.println("\n Inverted Pyramid 80 degree");
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // Innner loop --> space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Innner loop --> *
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
             System.out.println();
         }
     }
