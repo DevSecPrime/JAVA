a755cbff353008107b52632ab959bedf6e304af4
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-26 01:37:46 +0530

    pattern: inverted half pyramid with numbers

--- patterns/halfPyaramidNumber.java
+++ patterns/halfPyaramidNumber.java
@@ -14,7 +14,7 @@ public class halfPyaramidNumber {
         System.out.println("Half-Pyramid with number");
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             System.out.println();
         }
@@ -24,10 +24,24 @@ public class halfPyaramidNumber {
         // 333
         // 4444
         // 55555
-        System.out.println("Half-Pyramid with number");
+        System.out.println("\n Half-Pyramid with number inverted");
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(i);
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        // Inverted Half-Pyramid with number
+        // 1 2 3 4 5
+        // 1 2 3 4
+        // 1 2 3
+        // 1 2
+        // 1
+        System.out.println("Inverted Half-Pyramid with number");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
