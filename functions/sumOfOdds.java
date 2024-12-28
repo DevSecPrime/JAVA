430aaec06398f91971985e9ff33783b3ac8cc710
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-28 23:39:02 +0530

    function: add only odd number

---
+++ functions/sumOfOdds.java
@@ -0,0 +1,14 @@
+package functions;
+
+import java.util.Scanner;
+
+public class sumOfOdds {
+    public static void main(String[] args) {
+        // take input from the user
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // calling function to add odd numbers
+
+    }
+}
