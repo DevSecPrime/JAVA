71946d24f6b015a062ba56d323f3e8213633da42
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-28 20:16:23 +0530

    functions in java

---
+++ functions/printMyNameFumction.java
@@ -0,0 +1,23 @@
+package functions;
+
+import java.util.Scanner;
+
+public class printMyNameFumction {
+
+    public static void printMyName(String name) {
+
+        System.out.println(" your name is: " + name);
+        return;
+    }
+
+    public static void main(String args[]) {
+
+        // take input from user
+        System.out.print("Please enter your name:");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+
+        // Call function
+        printMyName(name);
+    }
+}
--- patterns/diamond.java
+++ patterns/diamond.java
@@ -35,7 +35,7 @@ public class diamond {
                 System.out.print(" ");
             }
 
-            for (int j = 0; j <= 2 * (i - 1); j++) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
