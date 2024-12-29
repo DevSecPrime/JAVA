68e713f851fe9ebdec0628ecc2297eb147a8cca8
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-29 13:50:04 +0530

    function: get count of +, -, 0 numbers

---
+++ functions/CountPositiveNegativeZeros.java
@@ -0,0 +1,45 @@
+package functions;
+
+import java.util.Scanner;
+
+public class CountPositiveNegativeZeros {
+    // Write a program to enter the numbers till the user wants and at the end it
+    // should display the count of positive, negative and zeros entered.
+
+    public static void getCountPositiveNegativeZeros(int n) {
+        int number;
+        int zerosCount = 0;
+        int negativeCount = 0;
+        int positiveCount = 0;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i <= n; i++) {
+            number = sc.nextInt();
+
+            if (number == 0) {
+                zerosCount++;
+
+            } else if (number < 0) {
+                negativeCount++;
+
+            } else {
+                positiveCount++;
+
+            }
+        }
+        System.out.println("The counts of Zero`s (0`s) is: " + zerosCount);
+        System.out.println("The counts of negative number is: " + negativeCount);
+        System.out.println("The counts of positive number is: " + positiveCount);
+    }
+
+    public static void main(String args[]) {
+        // take input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n inputs: ");
+        int n = sc.nextInt();
+
+        // call function
+        getCountPositiveNegativeZeros(n);
+
+    }
+
+}
