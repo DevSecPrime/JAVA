81e0bd901d437fff43a9da768bc802eaaf851eb2
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-30 21:27:15 +0530

    array: get array from user input

---
+++ arrays/ArrayUserInput.java
@@ -0,0 +1,38 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ArrayUserInput {
+    
+    //function to get array
+    public static void checkArray(int size) {
+
+        // initialize the array
+        int numbers[] = new int[size];
+
+        Scanner sc = new Scanner(System.in);
+        // loop for get input
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        // output loop
+        for (int i = 0; i < size; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        // take size of the array of the from the user
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of the array: ");
+        int size = sc.nextInt();
+
+        // call function
+        checkArray(size);
+
+    }
+
+}
