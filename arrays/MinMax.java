d358cf7f6c96cd8837fcccfb6f75fb2c957814cb
chiargiroid <chirag.l@iroidsolutions.com>
2024-12-31 19:27:18 +0530

    arrays: min-max elements from the arrays

---
+++ arrays/MinMax.java
@@ -0,0 +1,60 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class MinMax {
+
+    //find min and max element from the array
+    // Enter the size of the array:5
+    // -2 -6 5 9 2
+    // MAX ELEMENT OF THE ARRAY :9
+    // MIN ELEMENT OF THE ARRAY : -6
+
+    public static void findMinMaxOfArray(int size) {
+        Scanner sc = new Scanner(System.in);
+        // initialize the array
+        int numbers[] = new int[size];
+
+        // take input form the user
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        // get min & max values from the array
+
+        // int min = numbers[0];
+        // int max = numbers[0];
+
+        // for better approach in java
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < numbers.length; i++) {
+
+            ///check if 0th index value of array is less than min value
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+
+            ///check if 0th index value of array is greater than min value
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+
+        }
+
+        System.out.println("MAX ELEMENT OF THE ARRAY : " + max);
+        System.out.println("MIN ELEMENT OF THE ARRAY : " + min);
+    }
+
+    public static void main(String args[]) {
+        // Min Max problem in java
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        ///call function 
+        findMinMaxOfArray(size);
+
+    }
+}
